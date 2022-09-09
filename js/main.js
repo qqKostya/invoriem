240b39791f2afa78eb19fe141ebeb4ccc4acc96f
qqKostya <leap74rus@gmail.com>
2022-09-09 11:40:48 +0500

    add play and pause video by js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ $(document).ready(function(){
   }
   );
 });
+
+// Video
+
+const videoBtn = document.querySelector('#video-btn');
+const videoPic = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
+
+videoWrapper.addEventListener('click', function () {
+  if (video.paused) {
+    videoPic.classList.add('none');
+    videoWrapper.classList.remove('video-overlay');
+    videoBtn.classList.add('none');
+    video.play();
+  } else {
+    videoWrapper.classList.add('video-overlay');
+    videoBtn.classList.remove('none');
+    video.pause();
+  }
+})
